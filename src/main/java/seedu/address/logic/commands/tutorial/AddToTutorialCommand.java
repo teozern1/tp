5b7abdfcf882e6ede4a976f261c5323ecf342b1c
Propene <bingxi.bobby.song@gmail.com>
2023-10-18 18:18:43 +0800

    clean up checkstyle

--- src/main/java/seedu/address/logic/commands/tutorial/AddToTutorialCommand.java
+++ src/main/java/seedu/address/logic/commands/tutorial/AddToTutorialCommand.java
@@ -1,8 +1,9 @@
 package seedu.address.logic.commands.tutorial;
 
-import static seedu.address.logic.parser.CliSyntax.*;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_MODULE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TUTORIAL_NAME;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 import java.util.HashSet;
 import java.util.List;
@@ -15,12 +16,12 @@ import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
-import seedu.address.model.tag.Tag;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
+import seedu.address.model.tag.Tag;
 import seedu.address.model.tutorial.Tutorial;
 
 /**
