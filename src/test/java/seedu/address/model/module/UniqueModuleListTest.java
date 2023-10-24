63fa332dcb6cf320381c3d9350a81c7802efe10b
jacksonyuan256 <e0957476@u.nus.edu>
2023-10-24 14:44:01 +0800

    Rectify some checkstyle errors

--- src/test/java/seedu/address/model/module/UniqueModuleListTest.java
+++ src/test/java/seedu/address/model/module/UniqueModuleListTest.java
@@ -1,12 +1,13 @@
 package seedu.address.model.module;
 
-import org.junit.jupiter.api.Test;
-import seedu.address.model.module.exceptions.DuplicateModuleException;
-import seedu.address.model.module.exceptions.ModuleNotFoundException;
-
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.module.exceptions.DuplicateModuleException;
+import seedu.address.model.module.exceptions.ModuleNotFoundException;
 
 public class UniqueModuleListTest {
     private final UniqueModuleList uniqueModuleList = new UniqueModuleList();
