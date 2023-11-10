739631c07d6e93ff734f3e5c2e6a91abeea0cf58
teozern1 <75937968+teozern1@users.noreply.github.com>
2023-11-10 19:01:07 +0800

    Clear whitespace

--- src/test/java/seedu/address/logic/parser/AddCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddCommandParserTest.java
@@ -233,13 +233,13 @@ public class AddCommandParserTest {
         Person expectedPerson = new PersonBuilder(BOB).withTelegram("PlaceholderTelegramHandle").build();
         // no telegram handle
         assertParseSuccess(parser, NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB
-                         + TAG_DESC_HUSBAND + TAG_DESC_FRIEND +  STUDENT_NUMBER_DESC_BOB,
+                         + TAG_DESC_HUSBAND + TAG_DESC_FRIEND + STUDENT_NUMBER_DESC_BOB,
                 new AddCommand(expectedPerson));
 
         Person noStudentNumber = new PersonBuilder(BOB).withStudentNumber("PlaceholderStudentNumber").build();
         //no student number
         assertParseSuccess(parser, NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB
-                        + TAG_DESC_HUSBAND + TAG_DESC_FRIEND +  TELEGRAM_DESC_BOB,
+                        + TAG_DESC_HUSBAND + TAG_DESC_FRIEND + TELEGRAM_DESC_BOB,
                 new AddCommand(noStudentNumber));
     }
 }
